b8d33c643736a7a2d38ede3ca4b7e5655a1c2cda
Jakub Konka <kubkon@gmail.com>
2014-01-12 22:46:03 +0000

    Improve solution to nucleotide-count following nitpicks.

--- haskell/nucleotide-count/dna.hs
+++ haskell/nucleotide-count/dna.hs
@@ -4,17 +4,19 @@ module DNA (
 ) where
 
 
-import Data.Map.Strict (Map, fromList, mapWithKey)
+import Data.Map.Strict (Map, fromList, member, adjust)
+import Data.List (foldl')
 
 
 count :: Char -> String -> Int
 count c
-  | c `elem` allNucleotides = foldr (\x acc -> if x == c then acc + 1 else acc) 0
+  | c `elem` allNucleotides = foldl' (\acc x -> if x == c then acc + 1 else acc) 0
   | otherwise = error $ "invalid nucleotide " ++ show c
 
 nucleotideCounts :: String -> Map Char Int
-nucleotideCounts str = mapWithKey (\k x -> count k str + x) initMap
+nucleotideCounts = foldl' updateMap initMap
   where initMap = fromList $ zip nucleotidesInDNA $ replicate (length nucleotidesInDNA) 0
+        updateMap = flip $ adjust (1+)
 
 allNucleotides :: String
 allNucleotides = "ACGTU"
