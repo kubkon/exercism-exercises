96d962a2efc2d2856bae5aaf575cad41923c95fe
Jakub Konka <kubkon@gmail.com>
2014-01-12 13:02:28 +0000

    Implement solution to word-count problem in Haskell.

---
+++ haskell/word-count/WordCount.hs
@@ -0,0 +1,25 @@
+module WordCount (
+  wordCount,
+  ) where
+
+
+import Data.Char (toLower, isPunctuation, isSymbol)
+import qualified Data.Map as DM
+import qualified Data.Set as DS
+import Data.String.Utils (replace)
+
+
+wordCount :: String -> DM.Map String Int
+wordCount str = DM.fromList $ zip uniqueWords counts
+  where allWords = words $ sanitize str
+        uniqueWords = DS.toList $ DS.fromList allWords
+        counts = map (\f -> f allWords) $ map countElements uniqueWords
+
+sanitize :: String -> String
+sanitize = map replace
+  where replace c = if isPunctuation c || isSymbol c
+                      then ' '
+                      else toLower c
+
+countElements :: Eq a => a -> [a] -> Int
+countElements x = length . filter (\y -> y == x)
