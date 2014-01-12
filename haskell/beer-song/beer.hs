f1e23bd6af7cf39d2a9986537b58e22dc78b8515
Jakub Konka <kubkon@gmail.com>
2014-01-12 22:12:25 +0000

    Simplify beer-song according to nitpicks.

--- haskell/beer-song/beer.hs
+++ haskell/beer-song/beer.hs
@@ -11,16 +11,23 @@ sing :: Int -> Int -> String
 sing from to = unlines $ map verse [from,from-1..to]
 
 verse :: Int -> String
-verse n =  capitalize (numOfBottles n) ++ " of beer on the wall, " ++ numOfBottles n ++ " of beer.\n" ++ takeAction n ++ ", " ++ numOfBottles (n-1) ++ " of beer on the wall.\n"
-  where takeAction n
-          | n == 0 = "Go to the store and buy some more"
-          | n == 1 = "Take it down and pass it around"
-          | otherwise = "Take one down and pass it around"
+verse n = unlines [firstLine, secondLine]
+  where firstLine = unwords [capitalize (numOfBottles n),
+                             "of beer on the wall,",
+                             numOfBottles n,
+                             "of beer."]
+        secondLine = unwords [takeAction n,
+                              numOfBottles (n-1),
+                              "of beer on the wall."]
+        takeAction n
+          | n == 0 = "Go to the store and buy some more,"
+          | n == 1 = "Take it down and pass it around,"
+          | otherwise = "Take one down and pass it around,"
         numOfBottles n
-          | n == 0 = "no more " ++ pluralizedBottle
-          | n < 0 = show (100+n) ++ " " ++ pluralizedBottle
-          | otherwise = show n ++ " " ++ pluralizedBottle
-          where pluralizedBottle = pluralize "bottle" n
+          | n == 0 = unwords $ "no more" : pluralizedBottle
+          | n < 0 = unwords $ show (100+n) : pluralizedBottle
+          | otherwise = unwords $ show n : pluralizedBottle
+          where pluralizedBottle = [pluralize "bottle" n]
 
 capitalize :: String -> String
 capitalize "" = ""
