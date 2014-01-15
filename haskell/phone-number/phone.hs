65cd294fa77ddd5e37ae68da053a14b5a2af307a
Jakub Konka <kubkon@gmail.com>
2014-01-15 22:53:01 +0000

    First (naive) solution to phone number problem.

---
+++ haskell/phone-number/phone.hs
@@ -0,0 +1,34 @@
+module Phone (
+  areaCode,
+  number,
+  prettyPrint
+) where
+
+
+import Data.Char (isNumber)
+import Data.List.Split (splitPlaces)
+
+
+areaCode :: String -> String
+areaCode = take 3 . number
+
+number :: String -> String
+number = validate . filter isNumber
+
+validate :: String -> String
+validate "" = invalidNumber
+validate num
+  | check = reverse $ take 10 $ reverse num
+  | otherwise = invalidNumber
+  where check = length num > 9 && head num == '1'
+
+invalidNumber :: String
+invalidNumber = replicate 10 '0'
+
+prettyPrint :: String -> String
+prettyPrint = concat . prepare . splitPlaces [3, 3, 4] . number
+  where prepare (x:y:z:[]) = [
+                               "(" ++ x ++ ")"
+                             , " " ++ y ++ "-"
+                             , z
+                             ]
