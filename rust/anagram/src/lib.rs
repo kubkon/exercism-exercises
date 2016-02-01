f52cc867289725c424eba2ca3781506e95bf38e7
Jakub Konka <kubkon@jakubkonka.com>
2016-02-01 22:07:29 +0000

    Refactor anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn anagrams_for<'a>(string: &str, inputs: &[&'a str]) -> Vec<&'a str> {
-    let sort_string = |s: String| {
+    let sort = |s: String| {
         let mut chars = s.chars().collect::<Vec<char>>();
         chars.sort();
         chars
     };
-    let sorted_string = sort_string(string.to_lowercase());
     let allowed_inputs = inputs.iter().filter(|&s| s.to_lowercase() != string.to_lowercase());
-    allowed_inputs.filter(|&s| sort_string(s.to_lowercase()) == sorted_string)
+    allowed_inputs.filter(|&s| sort(s.to_lowercase()) == sort(string.to_lowercase()))
                   .fold(vec![], |mut acc, &s| { acc.push(s); acc })
 }
