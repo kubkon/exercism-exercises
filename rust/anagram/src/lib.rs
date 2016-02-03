50e803d004343743440a74152745c32386c349d0
Jakub Konka <kubkon@jakubkonka.com>
2016-02-03 22:21:23 +0000

    Replace fold() with cloned().collect()

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -6,5 +6,6 @@ pub fn anagrams_for<'a>(string: &str, inputs: &[&'a str]) -> Vec<&'a str> {
     };
     let allowed_inputs = inputs.iter().filter(|&s| s.to_lowercase() != string.to_lowercase());
     allowed_inputs.filter(|&s| sort(s.to_lowercase()) == sort(string.to_lowercase()))
-                  .fold(vec![], |mut acc, &s| { acc.push(s); acc })
+                  .cloned()
+                  .collect()
 }
