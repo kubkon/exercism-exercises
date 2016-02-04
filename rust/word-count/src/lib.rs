c8eae224a752e12785d78b72c4de8a29ad7df8da
Jakub Konka <kubkon@jakubkonka.com>
2016-02-04 22:36:32 +0000

    Reimplement word-count using regex

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,15 +1,19 @@
+extern crate regex;
+
+use regex::Regex;
 use std::collections::HashMap;
 
 pub fn word_count<'a>(s: &'a str) -> HashMap<String, u32> {
-    s.trim();
-    s.split_whitespace()
-     .fold(HashMap::new(), |mut acc, s| {
-         let key = s.to_string();
-         let count = match acc.get(&key) {
-             None => 1,
-             Some(count) => *count + 1,
-         };
-         acc.insert(key, count);
-         acc
+    let re = Regex::new("[^a-zA-Z0-9]+").unwrap();
+    re.split(s)
+      .filter(|s| !s.is_empty())
+      .fold(HashMap::new(), |mut acc, s| {
+          let key = s.to_lowercase().to_string();
+          let count = match acc.get(&key) {
+              None => 1,
+              Some(count) => *count + 1,
+          };
+          acc.insert(key, count);
+          acc
      })
 }
