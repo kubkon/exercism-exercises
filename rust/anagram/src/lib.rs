45527c568c50d825aa67863e7e0b3334e0b1a0c8
Jakub Konka <kubkon@jakubkonka.com>
2016-02-04 18:42:51 +0000

    Reduce number of repeated method calls in anagram exercise

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,12 +1,15 @@
 pub fn anagrams_for<'a>(string: &str, inputs: &[&'a str]) -> Vec<&'a str> {
-    let sort = |s: String| {
+    let sort = |s: &String| {
         let mut chars = s.chars().collect::<Vec<char>>();
         chars.sort();
         chars
     };
+    let string_lower = string.to_lowercase();
     inputs.iter()
-          .filter(|&s| s.to_lowercase() != string.to_lowercase())
-          .filter(|&s| sort(s.to_lowercase()) == sort(string.to_lowercase()))
+          .filter(|&s| {
+              let s_lower = s.to_lowercase();
+              s_lower != string_lower && sort(&s_lower) == sort(&string_lower)
+          })
           .cloned()
           .collect()
 }
