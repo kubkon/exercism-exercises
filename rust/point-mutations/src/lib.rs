a740e40c5fb7826ba93d1f8fb18aef28bfbf42d8
Jakub Konka <kubkon@jakubkonka.com>
2016-02-04 23:10:36 +0000

    Handle different string lenghts

--- rust/point-mutations/src/lib.rs
+++ rust/point-mutations/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn hamming_distance<'a>(left: &str, right: &str) -> Result<usize, &'a str> {
+    if left.len() != right.len() {
+        return Err("inputs of different length");
+    }
     Ok(0)
 }
