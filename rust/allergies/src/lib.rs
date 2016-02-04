8f477df47510d676b4621707a5e3092803a85f4c
Jakub Konka <kubkon@jakubkonka.com>
2016-02-04 19:25:46 +0000

    Make allergies exercise compile and tests run

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Clone,Debug,PartialEq)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+#[derive(Clone,Debug)]
+pub struct Allergies(pub usize);
+
+impl Allergies {
+    pub fn allergies(&self) -> Vec<Allergen> {
+        Vec::new()
+    }
+    
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        false
+    }
+}
