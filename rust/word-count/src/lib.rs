8b170701697b6f576ec7c72318788ef39149c492
Jakub Konka <kubkon@jakubkonka.com>
2016-02-04 22:20:35 +0000

    Implement basic word counter

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,5 +1,15 @@
 use std::collections::HashMap;
 
 pub fn word_count<'a>(s: &'a str) -> HashMap<String, u32> {
-    HashMap::new()
+    s.trim();
+    s.split_whitespace()
+     .fold(HashMap::new(), |mut acc, s| {
+         let key = s.to_string();
+         let count = match acc.get(&key) {
+             None => 1,
+             Some(count) => *count + 1,
+         };
+         acc.insert(key, count);
+         acc
+     })
 }
