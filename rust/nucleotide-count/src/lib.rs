5374f94fb4626824a784689f29350384a92356de
Jakub Konka <kubkon@jakubkonka.com>
2016-02-05 19:41:04 +0000

    Implement nucleotide-count exercise

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,9 +1,17 @@
 use std::collections::HashMap;
 
-pub fn count(c: char, s: &str) -> usize {
-    0
+pub fn count(ch: char, s: &str) -> usize {
+    s.chars().filter(|&c| c == ch).count()
 }
 
 pub fn nucleotide_counts(s: &str) -> HashMap<char, usize> {
-    HashMap::new()
+    let mut map = HashMap::new();
+    map.insert('A', 0);
+    map.insert('T', 0);
+    map.insert('C', 0);
+    map.insert('G', 0);
+    for (k, v) in map.iter_mut() {
+        *v = count(*k, s);
+    }
+    map
 }
